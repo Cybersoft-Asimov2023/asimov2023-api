8c269373d4adcc099ce8e0952990b717569dd4a6
Anderson Ore Aleman <u202011467@upc.edu.pe>
2023-09-22 09:32:51 -0500

    api test

--- src/test/java/com/cybersoft/asimovapi/AsimovApiApplicationTests.java
+++ src/test/java/com/cybersoft/asimovapi/AsimovApiApplicationTests.java
@@ -3,7 +3,3 @@ package com.cybersoft.asimovapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AsimovApiApplicationTests {
-
-}
